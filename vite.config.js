59b3e58e9d179c203ce618d6b0ba6ad9342b4dc6
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-07-04 03:08:52 -0700

    wrote a conditional in config if deploying using github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS
+    ? '/cv-application/'    // running on GH Actions → GitHub Pages
+    : '/',                  // anything else (Netlify, local dev) → root
   plugins: [react()],
 })
