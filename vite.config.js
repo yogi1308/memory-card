e4c30757cbbe366280c5e67372dc412514ff2a3e
Shreetej Hadge <shreetejhadge1308@gmail.com>
2025-07-04 03:33:53 -0700

    corrected config file

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: process.env.GITHUB_ACTIONS
-    ? '/cv-application/'    // running on GH Actions → GitHub Pages
+    ? '/memory-card/'    // running on GH Actions → GitHub Pages
     : '/',                  // anything else (Netlify, local dev) → root
   plugins: [react()],
 })
